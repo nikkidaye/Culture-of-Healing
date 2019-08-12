14c71031ddee4c7ee2d39fd49ba7027d07ccd9ec
Yolanda English <yolandaenglish@Yolandas-MacBook-Pro.local>
2019-08-12 11:45:02 -0400

    added initial components

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return(
+            <div className="cohhome">
+                <h1>Culture of Healing</h1>
+            </div>
+        )
+    }
+}
+export default Home;
